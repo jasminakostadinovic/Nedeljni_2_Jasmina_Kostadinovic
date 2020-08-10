ce3e265a34b3d6b904be11426d9e75b37c8a463c
jasminakostadinovic <jasmina.kostadinovic26@gmail.com>
2020-08-10 14:01:31 +0200

    MVVM folder structure

---
+++ Healthcare_App/ViewModel/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Healthcare_App.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
